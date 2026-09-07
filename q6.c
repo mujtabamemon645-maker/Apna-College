86e8d30bf0abb4899f6170528dbde44b299ead21
Muhammad Mujtaba <mujtabamemon645@gmail.com>
2026-09-07 08:21:35 -0700

    Add Question 6 of lab program

---
+++ q6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // Printing text with newline formatting
+    printf("Welcome to C Programming\n\n");
+    
+    // Printing student info layout
+    printf("Name:\tAli\n");
+    printf("Age:\t20\n");
+    printf("Grade:\tA\n\n");
+    
+    // Using \\ to successfully print backslashes in a file path
+    printf("File Path: C:\\Users\\Student\n\n");
+    
+    // Using \" to successfully print actual double quotes
+    printf("He said: \"Hello\"\n");
+
+    return 0;
+}
