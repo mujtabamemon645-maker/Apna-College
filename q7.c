42946a252c620b1a454b858e8f73b36d16f3e0ba
Muhammad Mujtaba <mujtabamemon645@gmail.com>
2026-09-07 08:24:19 -0700

    Add Question 7 of lab program

---
+++ q7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    // Sample Input
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    // Expected Output
+    printf("You entered: %c\n", ch);
+
+    return 0;
+}
